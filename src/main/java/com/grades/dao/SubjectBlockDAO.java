86b3497f86a909cd36ada790e528f5215a69bcc0
Wojciech Krokowski <wojkrokowski@gmail.com>
2017-01-24 12:34:38 +0100

    SubjectBlockDAO fixes

--- src/main/java/com/grades/dao/SubjectBlockDAO.java
+++ src/main/java/com/grades/dao/SubjectBlockDAO.java
@@ -9,20 +9,18 @@ import com.grades.dto.SubjectBlockDTO;
 @Repository
 public interface SubjectBlockDAO extends CrudRepository<SubjectBlockDTO, Long> {
 
-	@Query("select new com.grades.dto.SubjectBlockDTO(sb.subjectBlockId, sb.subjectId, sb.subjectFormId, sb.groupId, sb.workerId) from SubjectBlockDTO sb, "
-			+ "StudentSubjectDTO ss, StudentDTO s where ss.subjectBlockId = sb.subjectBlockId and s.indexNo=ss.indexNo and s.userId <> ?1")
+	@Query("select sb from SubjectBlockDTO sb where sb.subjectBlockId not in (select ss.subjectBlockId from StudentSubjectDTO ss, StudentDTO s "
+			+ "where ss.indexNo = s.indexNo and s.userId = ?1)")
 	Iterable<SubjectBlockDTO> findAllNonSelectedForStudent(Long userId);
 
-	@Query("select new com.grades.dto.SubjectBlockDTO(sb.subjectBlockId, sb.subjectId, sb.subjectFormId, sb.groupId, sb.workerId) from SubjectBlockDTO sb, "
-			+ "StudentSubjectDTO ss where ss.subjectBlockId = sb.subjectBlockId and ss.studentSubjectId = ?1")
+	@Query("select sb from SubjectBlockDTO sb, StudentSubjectDTO ss where ss.subjectBlockId = sb.subjectBlockId and ss.studentSubjectId = ?1")
 	SubjectBlockDTO findByStudentSubjectId(Long studentSubjectId);
 
-	@Query("select new com.grades.dto.SubjectBlockDTO(sb.subjectBlockId, sb.subjectId, sb.subjectFormId, sb.groupId, sb.workerId) from SubjectBlockDTO sb, "
-			+ "StudentSubjectDTO ss, StudentDTO s where ss.subjectBlockId = sb.subjectBlockId and s.indexNo=ss.indexNo and s.userId = ?1")
+	@Query("select sb from SubjectBlockDTO sb, StudentSubjectDTO ss, StudentDTO s where ss.subjectBlockId = sb.subjectBlockId "
+			+ "and s.indexNo=ss.indexNo and s.userId = ?1")
 	Iterable<SubjectBlockDTO> findAllSelectedForStudent(Long userId);
 
-	@Query("select new com.grades.dto.SubjectBlockDTO(sb.subjectBlockId, sb.subjectId, sb.subjectFormId, sb.groupId, sb.workerId) from SubjectBlockDTO sb, "
-			+ "WorkerDTO w where w.workerId = sb.workerId and w.userId = ?1")
+	@Query("select sb from SubjectBlockDTO sb, orkerDTO w where w.workerId = sb.workerId and w.userId = ?1")
 	Iterable<SubjectBlockDTO> findCreatedByWorker(Long userId);
 	
 	@Query("select count(*) from SubjectBlockDTO where subjectId = ?1 and subjectFormId = ?2 and groupId = ?3")
